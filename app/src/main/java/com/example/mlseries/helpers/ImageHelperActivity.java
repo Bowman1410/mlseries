db303a595fe73f3e408bf5c01c3426a83264a54e
Bowman1410 <iainbow1410@gmail.com>
2025-01-16 09:22:47 +0200

    Image Permissions & SDK Versions Control

--- app/src/main/java/com/example/mlseries/helpers/ImageHelperActivity.java
+++ app/src/main/java/com/example/mlseries/helpers/ImageHelperActivity.java
@@ -1,23 +1,42 @@
 package com.example.mlseries.helpers;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.Manifest;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mlseries.R;
 
+import java.io.IOException;
+
 public class ImageHelperActivity extends AppCompatActivity {
+
+    private int REQUEST_PICK_IMAGE = 1000;
+
+    private ImageView inputImageView;
+    private TextView outputTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_helper);
+
+        inputImageView = findViewById(R.id.imageViewInput);
+        outputTextView = findViewById(R.id.textViewOutput);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -32,5 +51,49 @@ public class ImageHelperActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         Log.d(ImageHelperActivity.class.getSimpleName(), "grant result for " + permissions[0] + " is " + grantResults[0]);
+    }
+
+    public void onPickImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
+
+    }
+
+    public void onStartCamera(View view) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            if (requestCode == REQUEST_PICK_IMAGE){
+                Uri uri = data.getData();
+                Bitmap bitmap = loadFromUri(uri);
+                inputImageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private Bitmap loadFromUri(Uri uri){
+        Bitmap bitmap = null;
+
+        try {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N_MR1){
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+
+            } else {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return bitmap;
     }
 }
